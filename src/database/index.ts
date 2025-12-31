5674b632b3d494593e96e6ae58247194e43bc380
eduardowentz <134004996+santoswentz@users.noreply.github.com>
2025-12-31 10:11:21 -0300

    Add TypeORM and SQLite database integration
    Integrated TypeORM with SQLite by adding configuration files, migration scripts, and database initialization. Updated dependencies in package.json to include TypeORM, reflect-metadata, sqlite3, and related packages. Modified src/index.ts to initialize the database connection.

---
+++ src/database/index.ts
@@ -0,0 +1,10 @@
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({ 
+    type: "sqlite",
+    database: "./src/database/db.sqlite",
+    migrations: [
+        "./src/database/migrations"
+    ],
+})
+ 
